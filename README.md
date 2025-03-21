6d3ee3c4da25b496aad45ee14f208793b9677a7b
lahgit <jesse.lahti@helsinki.fi>
2025-03-21 13:28:54 +0200

    ...

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Ohjelmistotekniikka, harjoitustyö
-## Aihe?
-mahdollisesti teen _jonkun pelin_
+## Monopoli tai sen kaltainen peli
+* pelaajat voi heittää noppaa ja liikkua pelilaudalla
+* pelaajilla on rahaa
 
 https://github.com/lahgit/ot-harjoitustyo/tree/master/laskarit
 
--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -2,7 +2,7 @@ import unittest
 from maksukortti import Maksukortti
 
 class TestMaksukortti(unittest.TestCase):
-    
+
     def setUp(self):
         self.kortti = Maksukortti(1000)
 
@@ -25,6 +25,29 @@ class TestMaksukortti(unittest.TestCase):
         kortti.syo_edullisesti()
 
         self.assertEqual(kortti.saldo_euroina(), 2.0)
+
+    def test_kortille_voi_ladata_rahaa(self):
+        self.kortti.lataa_rahaa(2500)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 35.0)
+
+    def test_kortin_saldo_ei_ylita_maksimiarvoa(self):
+        self.kortti.lataa_rahaa(20000)
+
+        self.assertEqual(self.kortti.saldo_euroina(), 150.0)
+    
+    def test_syo_maukkaasti_ei_vie_saldoa_negatiiviseksi(self):
+        kortti = Maksukortti(200)
+        kortti.syo_edullisesti()
+
+        self.assertEqual(kortti.saldo_euroina(), 2.0)
+
+    def test_negatiivinen_lataus_ei_lataa(self):
+        kortti = Maksukortti(200)
+        kortti2 = Maksukortti(200)
+        kortti.lataa_rahaa(-1)
+        self.assertEqual(kortti.saldo_euroina(), kortti2.saldo_euroina())
+
 
 
 
