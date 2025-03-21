ee52d5dab75dd5d69350930d5288ee4be542719f
lahgit <jesse.lahti@helsinki.fi>
2025-03-21 14:57:18 +0200

    teht 6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,24 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_alussa_oikea(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10)
+
+    def test_saldo_nousee_oikein(self):
+        self.maksukortti.lataa_rahaa(100)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 11)
+
+    def test_saldo_laskee_oikein(self):
+        self.maksukortti.ota_rahaa(200)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 8)
+
+    def test_saldo_ei_laske1(self):
+        self.maksukortti.ota_rahaa(1100)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10)
+
+    def test_saldo_ei_laske2(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1100), False)
+    
+    def test_saldo_ei_laske3(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1000), True)
