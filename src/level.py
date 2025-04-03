40423d5e69ca31c896164219e41fb9837fba7aeb
lahgit <jesse.lahti@helsinki.fi>
2025-04-03 14:56:35 +0300

    pylint, removed gap from tiles and textures by me

--- src/level.py
+++ src/level.py
@@ -8,25 +8,27 @@ class Level:
         self.level_map = level_map
         self.tileSize = 50
         self.GenerationSuccess = False
+        self.tile_texture = pygame.image.load("tiletexture.png")
+        self.flag_texture = pygame.image.load("flagtexture.png")
+        self.tile_texture = pygame.transform.scale(self.tile_texture, (self.tileSize, self.tileSize))
+        self.flag_texture = pygame.transform.scale(self.flag_texture, (self.tileSize, self.tileSize))
         
 
     def draw_grid(self): #Osittain generoitua koodia ChatGPT:llä
-        gapy = 0
+        #gapy = 0
         for row in range(len(self.level_map)):
-            gapx = 0
-            gapy += 5
+            #gapx = 0
+            #gapy += 5
             for col in range(len(self.level_map[row])):
-                gapx += 5
-                x = col * self.tileSize + gapx
-                y = row * self.tileSize + 60 + gapy 
+                #gapx += 5
+                x = col * self.tileSize #+ gapx
+                y = row * self.tileSize + 60 #+ gapy 
 
                 if self.level_map[row][col] == 1:
-                    color = (80, 50, 50)
-                else:
-                    color = (200, 200, 200)
-
-                pygame.draw.rect(self.display, color, (x, y, self.tileSize, self.tileSize))
-                pygame.draw.rect(self.display, (0, 0, 0), (x, y, self.tileSize, self.tileSize), 2) #Ja tähän loppuu ChatGPT:een koodi
+                    self.display.blit(self.flag_texture, (x, y))
+                if self.level_map[row][col] == 0:
+                    self.display.blit(self.tile_texture, (x, y))
+                #pygame.draw.rect(self.display, (20, 20, 20), (x, y, self.tileSize, self.tileSize), 2) #Ja tähän loppuu ChatGPT:een koodi
 
     
         self.GenerationSuccess = True
